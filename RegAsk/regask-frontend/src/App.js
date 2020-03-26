9c3098bec8d1238df792edb47c3c3b6b25129522
Jatin Anand <31433847+Jatin345Anand@users.noreply.github.com>
2020-03-26 13:30:22 +0530

    Add files via upload

--- RegAsk/regask-frontend/src/App.js
+++ RegAsk/regask-frontend/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+import Router from './routes/index'
+import AuthContainer from './containers/auth/authContainer';
+import HomeContainer from './containers/home/homeContainer';
+import {Button, Input, Form,Link,ListItem,ListItemIcon,ListItemText} from '@material-ui/core';
+// import {Button, Input, Form} from 'react-bootstrap';
+import AuthLandingOperation from './models/operations/rootLanding';
+import { BottomNavigation } from '@material-ui/core';
+import {NavLink} from 'react-router-dom';
+import Footer from './components/footer/coomonFooter';
+class App extends Component {
+  constructor(props){
+    super(props); 
+    this.isHome = false;
+    // this.isAuth = false;
+    
+    this.state ={
+      // type:AuthLandingOperation.type,
+      // type:'login',
+      type:'changepassword',
+      // type:'adduser',
+      username:'Super Admin'
+      // type:'login',
+      
+    }
+  }
+  render() {
+    return (
+      <div  > 
+      {/* <Router/> */} 
+      {/* <Button as={NavLink} to={'/home/'+this.state.username}  class="btn btn-primary"  >HOME</Button> */}
+      {/* <ListItem button component={NavLink} to="/home/JatinMarutiAnand">Home</ListItem> */}
+      {/* <NavLink to="/home/JatinMarutiAnand" activeClassName="active"  className="nav-link-item">   Home </NavLink> */}
+ 
+      {/* <NavLink to="/home/header/Maruti RajShish" activeClassName="active"  className="nav-link-item">   Home Header </NavLink> */}
+     {/* <BottomNavigation component={Footer}>FOOTER</BottomNavigation> */}
+      {this.isHome ?  <HomeContainer username={this.state.username}/> : <AuthContainer type={this.state.type} />}
+      
+     
+      </div>
+    );
+  }
 }
 
 export default App;
--- RegAsk/regask-frontend/src/index.js
+++ RegAsk/regask-frontend/src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../../../web-fonts-with-css/css/fontawesome-all.min.css';
+import RouterIndex from './routes/index';
+import {BrowserRouter} from 'react-router-dom';
+ReactDOM.render(<BrowserRouter><RouterIndex/></BrowserRouter> , document.getElementById('root'));
+registerServiceWorker();
