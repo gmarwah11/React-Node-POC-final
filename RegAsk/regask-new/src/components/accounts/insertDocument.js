0c3aaba387c0842cbac5475d19fabffa326375cf
Chandan Singh <csingh@placester.com>
2020-03-29 22:44:35 +0530

    code committed

--- RegAsk/regask-new/src/components/accounts/insertDocument.js
+++ RegAsk/regask-new/src/components/accounts/insertDocument.js
@@ -111,7 +111,7 @@ const useStyles = makeStyles(theme => ({
       data.append("market", this.state.market);
       data.append("language", this.state.language);
       data.append("country", this.state.country);
-      data.append("uploadedBy", this.state.uploadedBy);
+      data.append("uploadedBy", 1);
       data.append("uploadDate", new Date());
 
       axios.post("http://localhost:4000/api/document/upload", data).then(res => {
